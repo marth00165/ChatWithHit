d8494f2e516483d139a7e0a2c3c6b1d79eec5c64
Rohit <rohitpratti@gmail.com>
2021-01-13 22:13:43 -0700

    refactored Home added messages context

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Row, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import { useAuthDispatch } from '../../context/auth';
+
+import Users from './Users';
+import Messages from './Messages';
+
+const Home = ({ history }) => {
+  const dispatch = useAuthDispatch();
+
+  const logout = () => {
+    dispatch({ type: 'LOGOUT' });
+    history.push('/login');
+  };
+
+  return (
+    <>
+      <Row className="bg-white justify-content-around mb-1">
+        <Link to="/login">
+          <Button variant="link">Login</Button>
+        </Link>
+        <Link to="/register">
+          <Button variant="link">Register</Button>
+        </Link>
+        <Button variant="link" onClick={logout}>
+          Logout
+        </Button>
+      </Row>
+      <Row className="bg-white">
+        <Users />
+        <Messages />
+      </Row>
+    </>
+  );
+};
+
+export default Home;
