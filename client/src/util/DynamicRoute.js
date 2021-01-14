0e8163095fe0a4d9a25005c29eb14eb6251140c2
Rohit <rohitpratti@gmail.com>
2021-01-13 20:57:38 -0700

    context set up messages backend set up

---
+++ client/src/util/DynamicRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
+import { useAuthState } from '../context/auth';
+
+const DynamicRoute = (props) => {
+  const { user } = useAuthState();
+
+  if (props.authenticated && !user) {
+    return <Redirect to="/login" />;
+  } else if (props.guest && user) {
+    return <Redirect to="/" />;
+  } else {
+    return <Route component={props.component} {...props} />;
+  }
+};
+export default DynamicRoute;
